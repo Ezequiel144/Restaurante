cf716707dea781faac61fc3561783d40bf72a9eb
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-03-23 15:47:02 -0300

    commit 2

--- app.js
+++ app.js
@@ -4,36 +4,42 @@ const menu = [
         title:"Buttermilk Pancakes",
         text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
         price: 15.99,
+        tipo: "breakfast",
+    },
+    {
+        img:"./img/2.jpg",
+        title:"Smoothies",
+        text:"Smoothies are some of the best breakfast recipes, as they pack a big serving of fruits and veggies into your first meal of the day",
+        price: 25.99,
+        tipo: "breakfast",
+    },
+    {
+        img:"./img/3.jpg",
+        title:"Muffins",
+        text:"When I was growing up, chocolate chip muffins were a breakfast staple in our house. Muffins are still one of my favorite ways to start the day.",
+        price: 15.00,
+        tipo: "breakfast",
     },
     {
         img:"./img/1.jpg",
         title:"Buttermilk Pancakes",
         text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
-        price: 15.99,
+        price: 41.50,
+        tipo: "lunch",
     },
     {
         img:"./img/1.jpg",
         title:"Buttermilk Pancakes",
         text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
-        price: 15.99,
+        price: 20.99,
+        tipo: "lunch",
     },
     {
         img:"./img/1.jpg",
         title:"Buttermilk Pancakes",
         text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
-        price: 15.99,
-    },
-    {
-        img:"./img/1.jpg",
-        title:"Buttermilk Pancakes",
-        text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
-        price: 15.99,
-    },
-    {
-        img:"./img/1.jpg",
-        title:"Buttermilk Pancakes",
-        text:"Buttermilk pancakes, or pancakes, are the perfect breakfast. Buttermilk is whey. It is an ingredient that is widely used, especially in the United States, to make many types of desserts.",
-        price: 15.99,
+        price: 93.50,
+        tipo: "shakes",
     },
 ]
 
@@ -41,8 +47,9 @@ const menu = [
 const contSec1 = document.querySelector(".sec1__content");
 const listLink = document.querySelectorAll(".list__art__li__a");
 
-function creatMenu(){
-    for (const m of menu){
+function creatMenu(menus){
+    contSec1.innerHTML = "";
+    for (const m of menus){
         const div = document.createElement("div");
         div.className = "sec1__content__mod";
         div.innerHTML = `
@@ -58,7 +65,7 @@ function creatMenu(){
     }
 }
 
-creatMenu();
+creatMenu(menu);
 
 listLink.forEach(el => {
     /* console.log(el); */
@@ -67,7 +74,14 @@ listLink.forEach(el => {
             link.classList.remove("selec");
         }
         e.currentTarget.className = "list__art__li__a selec";
-        console.log(e.currentTarget);
+        /* console.log(e.currentTarget.id); */
+        if(e.currentTarget.id != "all"){
+            const menutip = menu.filter(m => m.tipo === e.currentTarget.id);
+            creatMenu(menutip);
+        }else{
+            creatMenu(menu);
+        }
+        /* console.log(e.currentTarget); */
     })
 });
 
